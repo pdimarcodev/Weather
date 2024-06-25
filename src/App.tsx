52ea75863a795545e4d2f86ca0522e64c521ca38
pdimarcodev <pablo.d.dimarco@gmail.com>
2024-06-25 18:17:12 +0200

    feat: inital setup

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from './presentation/router/router';
+
+export const App = () => {
+  return <RouterProvider router={router} />;
+};
